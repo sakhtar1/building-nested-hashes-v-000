dee525d3eb26deede654bd9e0041c1c1f68ab55a
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 20:26:32 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
-        {name: "nurse", age: "44", attitude: "worried"}
+        {name: "Nurse", age: "44", attitude: "worried"}
         #your hashes here!
       ]
    }
